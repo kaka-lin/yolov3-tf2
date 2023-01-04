98ba95bf9749aa08416861d27f66df67beade263
kaka <vn503024@gmail.com>
2023-01-04 15:34:28 +0800

    chore: add script files
    1. run docker
    2. run jupyter notebook

--- run.sh
+++ run.sh
@@ -2,11 +2,32 @@
 
 xhost +local:docker
 
-docker run -it \
-    --gpus all \
+DOCKER_REPO="kakalin/"
+BRAND=kimage
+VERSION=cuda11.6-tf2.9.1-devel
+
+IMAGE_NAME=${DOCKER_REPO}${BRAND}:${VERSION}
+
+docker_run_params=$(cat <<-END
+    --rm \
+    -it \
     -e DISPLAY=$DISPLAY \
     -e QT_X11_NO_MITSHM=1 \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix" \
-    --volume="$PWD:/root/yolov3-tf2" \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
     --network=host \
-    kakalin/kimage:cuda11.6-tf2.9.1-devel
+    -p 8888:8888 \
+    -v $PWD:/root/yolov3-tf2 \
+    -w /root/yolov3-tf2 \
+    $IMAGE_NAME
+END
+)
+
+if [[ -z "$1" ]]; then
+    docker run \
+        --gpus all \
+        $docker_run_params
+else
+    docker run \
+        --gpus '"device='$1'"' \
+        $docker_run_params
+fi
